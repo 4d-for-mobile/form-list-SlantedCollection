605a67574bf326366209b0194c96db679de64cde
David Azancot <david.azancot@4d.com>
2020-07-01 16:28:56 +0200

    Update sdk Fix action button visibility

--- Sources/Forms/Tables/___TABLE___/___TABLE___ListForm.swift
+++ Sources/Forms/Tables/___TABLE___/___TABLE___ListForm.swift
@@ -13,20 +13,20 @@ import CollectionViewSlantedLayout
 @IBDesignable
 
 class ___TABLE___ListForm: ListFormCollection {
-
+    
     // Do not edit name or override tableName
     public override var tableName: String {
         return "___TABLE___"
     }
-
+    
     let layout = CollectionViewSlantedLayout()
     let actionButton = UIButton()
-
+    
     // MARK: Events
     override func onLoad() {
         // Do any additional setup after loading the view.
         navigationController?.isNavigationBarHidden = true
-
+        
         // Action Button definition
         actionButton.frame = CGRect(x: screenWidth - 70, y: 45, width: 50, height: 50)
         let image = UIImage(named: "moreButton")
@@ -34,10 +34,10 @@ class ___TABLE___ListForm: ListFormCollection {
         actionButton.actionSheet = self.actionSheet
         actionButton.transform = CGAffineTransform(scaleX: 0, y: 0)
         view.addSubview(actionButton)
-
+        
         // SearchBar text style
         let blueColor = UIColor(red: 90/255, green: 179/255, blue: 174/255, alpha: 1.0)
-
+        
         // Floating SearchBar definition
         searchBar = UISearchBar(frame: CGRect(x: 20, y: 50, width: UIScreen.main.bounds.width - 100, height: 40))
         searchBar.layer.cornerRadius = 10
@@ -48,12 +48,12 @@ class ___TABLE___ListForm: ListFormCollection {
         let barButtonAppearanceInSearchBar: UIBarButtonItem?
         barButtonAppearanceInSearchBar = UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self])
         barButtonAppearanceInSearchBar?.tintColor = blueColor
-
+        
         // SearchBar text style
         let textFieldInsideUISearchBar = searchBar.value(forKey: "searchField") as? UITextField
         textFieldInsideUISearchBar?.textColor = blueColor
         textFieldInsideUISearchBar?.font = UIFont(name: "HelveticaNeue-Thin", size: 15)
-
+        
         self.refreshControl?.tintColor = blueColor
         searchBar.searchBarStyle = UISearchBar.Style.prominent
         searchBar.isTranslucent = false
@@ -61,13 +61,16 @@ class ___TABLE___ListForm: ListFormCollection {
         searchBar.delegate = self
         self.view.addSubview(searchBar)
     }
-
+    
     override func onWillAppear(_ animated: Bool) {
         // Called when the view is about to made visible. Default does nothing
         collectionView.collectionViewLayout.invalidateLayout()
         collectionView.contentInset.top = -UIApplication.shared.statusBarFrame.height
+        if actionSheet == nil {
+            self.actionButton.isHidden = true
+        }
     }
-
+    
     override func onDidAppear(_ animated: Bool) {
         // Called when the view has been fully transitioned onto the screen. Default does nothing
         UIView.animate(withDuration: 1.0, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: [.curveEaseOut], animations: {
@@ -78,21 +81,21 @@ class ___TABLE___ListForm: ListFormCollection {
             self.actionButton.transform = .identity
         }, completion: nil)
     }
-
+    
     override func onWillDisappear(_ animated: Bool) {
         // Called when the view is dismissed, covered or otherwise hidden. Default does nothing
     }
-
+    
     override func onDidDisappear(_ animated: Bool) {
         // Called after the view was dismissed, covered or otherwise hidden. Default does nothing
     }
-
+    
     open override func onSearchFetching() {
         collectionView?.collectionViewLayout = layout
         layout.isFirstCellExcluded = true
         layout.isLastCellExcluded = true
     }
-
+    
     open override func onSearchCancel() {
         collectionView?.collectionViewLayout = layout
         layout.isFirstCellExcluded = true
@@ -102,7 +105,7 @@ class ___TABLE___ListForm: ListFormCollection {
 
 // MARK: Layout
 extension ___TABLE___ListForm: CollectionViewDelegateSlantedLayout {
-
+    
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard let collectionView = collectionView else {return}
         guard let visibleCells = collectionView.visibleCells as? [___TABLE___CustomCollectionCell] else {return}
